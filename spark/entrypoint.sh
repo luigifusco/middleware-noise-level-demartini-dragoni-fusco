faee79c0af32f03066d44027fbf016741f3ca7f4
imDema <luca.de.martini@protonmail.com>
2022-03-15 15:17:37 +0100

    Add print version at master stat

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SPARK_HOME=/spark
+cd /spark/bin/
 
 if [ "$SPARK_MODE" == "master" ]; then
     SPARK_MASTER_HOST=${SPARK_MASTER_HOST:-`hostname`}
@@ -9,9 +10,10 @@ if [ "$SPARK_MODE" == "master" ]; then
     SPARK_MASTER_LOG=/spark/logs
 
     echo "** Starting Spark in master mode **"
-    cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
+    ./spark-class org.apache.spark.deploy.master.Master \
         --host $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
 else
     echo "** Starting Spark in worker mode with master $SPARK_MASTER **"
-    spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER
+    ./spark-shell --version
+    ./spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER
 fi
