852dba5853abbdfa96dbab8332bab9823f0f0222
imDema <luca.de.martini@protonmail.com>
2022-03-16 12:00:03 +0100

    Update docker-compose and spark entrypoint.sh

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SPARK_HOME=/spark
-cd /spark/bin/
+cd /data/
 
 if [ "$SPARK_MODE" == "master" ]; then
     SPARK_MASTER_HOST=${SPARK_MASTER_HOST:-`hostname`}
@@ -10,10 +10,10 @@ if [ "$SPARK_MODE" == "master" ]; then
     SPARK_MASTER_LOG=/spark/logs
 
     echo "** Starting Spark in master mode **"
-    ./spark-class org.apache.spark.deploy.master.Master \
+    /spark/bin/spark-class org.apache.spark.deploy.master.Master \
         --host $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
 else
     echo "** Starting Spark in worker mode with master $SPARK_MASTER **"
-    ./spark-shell --version
-    ./spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER
+    /spark/bin/spark-shell --version
+    /spark/bin/spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER
 fi
